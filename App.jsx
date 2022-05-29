f403ad20ff4103878dcd8321f826f49cb47bedcd
Ibrahim Yakubu <ibrahimy437@gmail.com>
2022-05-29 13:22:40 +0100

    minor update

--- App.jsx
+++ App.jsx
@@ -3,25 +3,55 @@ import { AuthClient } from "@dfinity/auth-client";
 
 const init = () => {
   async function handleClick() {
-    const [sayHello, setSayHello] = useState();
+    const [login, setLogin] = useState();
+    const [username, setUsername] = useState("");
     const authClient = await AuthClient.create();
 
-    if (await authClient.isAuthenticated()) {
-      handleAuthenticated(authClient);
-    } else {
-      await authClient.login({
-        identityProvider: "https://identity.ic0.app/#authorize",
-        onSuccess: () => {
-          handleAuthenticated(authClient);
-        },
-      });
+    async function signUp() {
+      if (await authClient.isAuthenticated()) {
+        const identity = client.getIdentity();
+        const principal = identity.getPrincipal().toString();
+        setSignedIn(true);
+        setUsername(principal);
+      } else {
+        await authClient.login({
+          identityProvider: "https://identity.ic0.app/#authorize",
+          onSuccess: () => {
+            const identity = client.getIdentity();
+            const principal = identity.getPrincipal().toString();
+            setLogin(true);
+            setUsername(principal);
+          },
+        });
+      }
     }
   }
 
-  async function handleAuthenticated(authClient) {
-    const greeting = await main.greet(authClient.toText());
-    setSayHello(greeting.toText());
-  }
+  // async function handleAuthenticated(authClient) {
+  //   const greeting = await main.greet(authClient.toText());
+  //   setSayHello(greeting.toText());
+  // }
 
-  return <span onClick={handleClick}>{sayHello}</span>;
+  const logout = async () => {
+    await client.logout();
+    setLogin(false);
+    setUsername("");
+  };
+
+  useEffect(() => {
+    initAuth();
+  }, []);
+
+  return (
+    <div>
+      {!login && client ? <span onClick={signUp}>Sign up</span> : null}
+
+      {signedIn ? (
+        <>
+          <p>Signed in as: {username}</p>
+          <button onClick={logout}>Sign out</button>
+        </>
+      ) : null}
+    </div>
+  );
 };
